09971a012be9040dd046dcf61a36c620802d4b8e
Leonardo <45024471+LEMNXCIX@users.noreply.github.com>
2024-03-11 13:28:01 -0500

    validacion de variables externa

--- helpers/validateVariables.js
+++ helpers/validateVariables.js
@@ -1,9 +1,13 @@
+/**
+ * Se valida si es un objeto que no se encuentra vacio
+ * 
+ * @param {Object} value 
+ * @returns Boolean
+ */
+function isEmptyObject(value) {
+  return (
+    value && Object.keys(value).length === 0 && value.constructor === Object 
+  );
+}
 
-//class Validate{
-    function isEmptyObject(value) {
-    return value && Object.keys(value).length === 0 && value.constructor === Object;
-  }
-//}
-
-  //module.exports = Validate;
-  module.exports = {isEmptyObject}
+module.exports = { isEmptyObject };
